f067d0f641fd1ac1aa99212cb8fd5a5e4a724b2d
Duraimurugan-H <hduraimurugan@gmail.com>
2024-07-19 20:42:24 +0530

    Updated some functions reset buttons,  input in any fields and result in alternate fields

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let select = document.querySelectorAll('.currency')
 let btn = document.getElementById('btn')
+let btnReset= document.getElementById('btn-reset')
 let input = document.getElementById('input')
 let error=document.querySelector('.error')
+let result=document.getElementById('result')
 
 fetch('https://api.frankfurter.app/currencies')
 .then(res=>res.json())
@@ -21,19 +23,47 @@ btn.addEventListener('click',()=>{
   let curr1 = select[0].value
   let curr2 = select[1].value
   let inputVal = input.value
+  let resVal= result.value
   if(curr1===curr2){
     error.classList.add('show')
+    error.innerText='⚠️Kindly Choose different currencies and try again...'
   }   
-  else
+
+  if(inputVal=="" && resVal==""){
+    error.classList.add('show')
+    error.innerText='⚠️Please enter data to calculate...'
+  }
+  else if(!inputVal=="" && !resVal==""){
+    error.classList.add('show')
+    error.innerText='⚠️Kindly enter data in any one box only, Reset and try again...'
+  }
+  else if(resVal=="")
     convert(curr1,curr2,inputVal)
+
+  else if(inputVal=="")
+    convertt(curr2,curr1,resVal)
 });
+
+btnReset.addEventListener('click',()=>{
+  result.value="";
+  input.value ="";
+  error.classList.remove('show')
+})
 
 function convert(curr1,curr2,inputVal){
   fetch(`https://api.frankfurter.app/latest?amount=${inputVal}&from=${curr1}&to=${curr2}`)
   .then(resp => resp.json())
   .then((data) => {
-    console.log(data.rates)
-    document.getElementById('result').value = Object.values(data.rates)[0]
+    result.value = Object.values(data.rates)[0]
+  });
+
+}
+
+function convertt(curr1,curr2,inputVal){
+  fetch(`https://api.frankfurter.app/latest?amount=${inputVal}&from=${curr1}&to=${curr2}`)
+  .then(resp => resp.json())
+  .then((data) => {
+    input.value = Object.values(data.rates)[0]
   });
 
 }
